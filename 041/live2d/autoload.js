d002181c8819b9c29c1bd45e4a8c51786ee4d947
古馨月 <105254396+lishi19990812@users.noreply.github.com>
2022-11-14 23:41:08 +0800

    live2d
    live2d api

---
+++ 041/live2d/autoload.js
@@ -0,0 +1,27 @@
+// 加载 CSS
+$("<link>").attr({href: "https://jx.teed.cc:8443/live2d/waifu.css", rel: "stylesheet", type: "text/css"}).appendTo('head');
+
+// 插入 DIV
+$('body').append('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span> <span class="fui-chat"></span> <span class="fui-eye"></span> <span class="fui-user"></span> <span class="fui-photo"></span> <span class="fui-info-circle"></span> <span class="fui-cross"></span></div></div>');
+
+// 加载 JS
+$.ajax({
+	url: 'https://jx.teed.cc:8443/live2d/waifu-tips.js',
+	dataType:"script",
+	cache: true,
+	async: false
+});
+$.ajax({
+	url: 'https://jx.teed.cc:8443/live2d/live2d.js',
+	dataType:"script",
+	cache: true,
+	async: false
+});
+
+// 初始化看板娘，加载 waifu-tips.json
+
+/* 可直接修改部分参数 */
+//    live2d_settings['modelId'] = 5;                // 默认模型 ID
+//    live2d_settings['modelTexturesId'] = 1;        // 默认材质 ID
+/* 在 initModel 前添加 */
+initModel('https://jx.teed.cc:8443/live2d/waifu-tips.json');
